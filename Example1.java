6e7fdd92d6f91de6644d35ed21f2d4ebb0858953
lenpelletier <lenpelletier@gmail.com>
2026-09-14 14:50:34 -0700

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,9 @@
+public class Example1 {
+
+	public static void main (String[] args) {
+		String name = "Mr. P";
+		int age = 47; 
+		System.out.println("My name is " + name + " and I am " + age + ".");
+		
+	}
+}
